231805d32c8f9e7740cca62d5b35d0c9f7b12840
LeoNitka <lhnmda@gmail.com>
2022-10-28 22:18:30 -0300

    javascript19

---
+++ scripts/login.js
@@ -0,0 +1,85 @@
+// GET ID DE FORMULARIO
+let formLogin = document.getElementById("formLogin");
+
+// GET DATOS DEL USUARIO DESDE LOCALSTORAGE
+const userData = JSON.parse(localStorage.getItem("userData")) || [];
+
+// VARIABLE QUE ALMACENA LOS INTENTOS DE INICIO DE SESIÓN
+let intentos = 3;
+
+// FUNCION VALIDACIÓN DE DATOS PARA EL INICIO DE SESIÓN
+function validateLogin() {
+
+    // GET INPUT VALUES DEL FORMULARIO DE INICIO DE SESIÓN
+    let ingresePass = document.getElementById("passLogin").value;
+	let ingreseEmail = document.getElementById("emailLogin").value;
+
+	// ACCESO A LAS PROPIEDADES DEL OBJETO USUARIO
+	const email = userData.email;
+    const pass = userData.pass;
+
+	// COMPARACIÓN ENTRE LOS DATOS DEL USUARIO Y LOS VALORES DE LOS INPUTS DE LOGIN
+	const hayDatosInvalidos = (password, emailUser) => password != pass || emailUser != email;
+
+
+    if (!hayDatosInvalidos(ingresePass, ingreseEmail)) {
+    	Swal.fire(
+		'Sesion iniciada',
+		'exitos!',
+		'success'
+	  )
+
+    } else {
+		Swal.fire(
+			'contraseña o email equivocada',
+			`${intentos-1} ${intentos === 2 ?  'intento' : 'intentos'}`,
+			'question'
+		)
+    
+	
+        if(intentos === 1){
+			Swal.fire({
+				icon: 'error',
+				title: 'contraseña y email incorrectos',
+				text: 'intente nuevamente mas tarde',
+				
+			})
+
+            // GET POR ID DEL BUTTON 'LOGIN' DEL FORMULARIO DE INICIO DE SESIÓN, QUE SE DESHABILITA
+            // UNA VEZ QUE EL USUARIO AGOTA LOS INTENTOS DE INICIO DE SESIÓN.
+            document.getElementById('btnInputLogin').disabled = 'true';
+        }
+        intentos--;
+
+		
+    }
+}
+
+// EVENTO INICIO DE SESIÓN DE USUARIO
+formLogin.addEventListener("submit", (e)=>{
+	e.preventDefault();
+
+    // LLAMADA A LA FUNCIÓN PARA VALIDAR DATOS
+	validateLogin();
+  }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
